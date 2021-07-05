29ae28601c01a11d533b944175083860fc9d715c
Jose Reis <jccreis@hotmail.com>
2021-07-05 09:48:52 +0100

    Funcão random FIXED

--- world.py
+++ world.py
@@ -28,8 +28,8 @@ class StartTile(MapTile):
     def intro_text(self):
         return """
         Na tua frente tens uns portões ferruguentos que aparentam ter centenas de anos desde que foram abertos
-        que misterios, lendas, monstros e tesouros se escondem lá dentro
-        irás descobrir logo que abras os mesmos!!
+        que misterios, lendas, monstros e tesouros se escondem lá dentro!!!
+        Irás descobrir logo que abras os mesmos!!
         """
         
 class DragonTile(MapTile):
@@ -41,45 +41,45 @@ class DragonTile(MapTile):
                 
 class EnemyTile(MapTile):
     def __init__(self, x, y):
-        r = random.random()
-        if r < 0.95:
+        r = random.randint(1,10)
+        if r < 5:
             self.enemy = enemies.GiantSpider()
             self.alive_text = "\nUma Aranha gigante salta a tua frente " \
                 "e lança a teia contra ti"
             self.dead_text = "\nO corpo morto da aranha " \
                 "apodrece no chão."
-        elif r < 0.80:
+        elif r < 8:
             self.enemy = enemies.Ogre()
             self.alive_text = "\nUm Ogre está a bloquear o teu caminho!"
             self.dead_text = "\nUm ogre morto no chão relembra-te do teu triunfo"
-        elif r < 0.50:
+        elif r < 6:
             self.enemy = enemies.BatColony()
             self.alive_text = "\nOuves uns barulhos ao longe" \
                 "... até que de repente te vês no meio de um enxame de morcegos!"
             self.dead_text = "\nDezenas de morcegos estão espalhados no chão."
-        elif r < 0.35:
+        elif r < 3:
             self.enemy = enemies.Bear()
             self.alive_text = "\n Vês um enorme Urso Pardo" \
                 "de pé e com cara de quem te quer atacar"
             self.dead_text = "Ficas todo contente ao ver que o urso jaz inaminado no chão"
             
-        elif r < 0.30:
+        elif r < 2 :           
             self.enemy = enemies.Goblin()
             self.alive_text = "Um Goblin verde e verruguento armado com uma espada"
             self.dead_text = "matas o Goblin e vês o sangue verde a escorrer pelo chão"
             
-        elif r <0.10:
-            self.enemy = enemies.Dragon()
-            self.alive_text = """Encontras-te um dragão gigante. Apesar de todas as tuas tentativas
-        morres queimado pelo mesmo.
-        Foi uma boa viagem, mas acaba aqui.""" 
-        
-        else:
+        elif r < 1:
             self.enemy = enemies.RockMonster()
             self.alive_text = "\nTu acordaste um monstro de pedra " \
                 "no seu covil!"
             self.dead_text = "\nDerrotado, o monstro reverteu " \
                 "para uma rocha normal."
+                    
+        else:
+            self.enemy = enemies.Dragon()
+            self.alive_text = """Encontras-te um dragão gigante. Apesar de todas as tuas tentativas
+        morres queimado pelo mesmo.
+        Foi uma boa viagem, mas acaba aqui.""" 
             
         super().__init__(x, y)
         
@@ -97,23 +97,23 @@ class EnemyTile(MapTile):
 class ItemTile(MapTile):
     def __init__(self, x, y):
         super().__init__(x,y)
-        r = random.random()
-        #if r < 79:
-        #    self.item = items.CrustyBread()
-        #    self.description = """  \nEncontras-te um pão duro no chão"""
-        if r < 80:
+        r = random.randint(1,10)
+        if r < 5:
+            self.item = items.CrustyBread()
+            self.description = """  \nEncontras-te um pão duro no chão"""
+        if r < 7:
             self.item = items.Apple()
             self.description = """ \nOlhas para a sala e vês uma maçã"""
         
-        elif r < 70:
+        elif r < 3:
             self.item = items.HealingPotion()
             self.description = """\nUma poção de vida, que maravilha"""
             
-        elif r <65:
+        elif r <8:
             self.item = items.RustySword()
             self.description = """\nEncontras-te uma espada, vai-te dar bastante jeito"""
         else:
-            print("\nNão encontraste nada!!")
+             self.description = """\nNão encontraste nada!!"""
             
     def intro_text(self):
         text = self.description 
